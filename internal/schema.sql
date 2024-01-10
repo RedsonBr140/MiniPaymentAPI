b78bf978359daa6e856efe7f7555f999bdf146fe
Redson <redson@riseup.net>
2024-01-10 10:13:12 -0300

    chore(db): delete schema.sql as it's useless now

--- internal/schema.sql
+++
@@ -1,9 +0,0 @@
--- Create enum type "funcao"
-CREATE TYPE "funcao" AS ENUM ('COMUM', 'LOJISTA');
--- Create "users" table
-CREATE TABLE "users" ("user_id" uuid NOT NULL DEFAULT gen_random_uuid(), "first_name" character varying(255) NOT NULL, "last_name" character varying(255) NOT NULL, "cpf" character varying(14) NOT NULL, "email" character varying(255) NOT NULL, "user_type" "funcao" NOT NULL, PRIMARY KEY ("user_id"));
--- Create index "users_cpf_key" to table: "users"
-CREATE UNIQUE INDEX "users_cpf_key" ON "users" ("cpf");
--- Create index "users_email_key" to table: "users"
-CREATE UNIQUE INDEX "users_email_key" ON "users" ("email");
-CREATE TABLE "transactions" ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "payer" uuid NOT NULL, "payee" uuid NOT NULL, "value" VARCHAR(255) NOT NULL)
